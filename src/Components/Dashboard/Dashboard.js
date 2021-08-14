4332bc0379284f9c44e889c4e30745d9873bd6a7
hansitharaka <it19188850@my.sliit.lk>
2021-08-14 15:55:15 +0530

    Added components and linked to the side navigation

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+import {Button, Card, Container} from "react-bootstrap";
 
 class Dashboard extends Component {
     constructor(props) {
@@ -9,10 +12,24 @@ class Dashboard extends Component {
         }
     }
 
+    componentDidMount() {
+        console.log(window.location.pathname)
+    }
+
 
     render() {
         return(
-            <div>DASHBOARD</div>
+            <div>
+                <div>
+                    <Container className={"my-5"}>
+                        <Card>
+                            <Card.Body>
+                                <h1>Dashboard</h1>
+                            </Card.Body>
+                        </Card>
+                    </Container>
+                </div>
+            </div>
         )
     }
 
--- src/Components/Frontend.js
+++ src/Components/Frontend.js
@@ -2,16 +2,43 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import Home from "./Home/Home";
+import Sidebar from "./SideBar/Sidebar";
+import ViewLibrary from "./Library/ViewLibrary";
+import Dashboard from "./Dashboard/Dashboard";
+import Classroom from "./Classroom/Classroom";
+import Notices from "./Notice/Notices";
+import Users from "./User/Users";
 
 class Frontend extends Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    componentDidMount() {
+        console.log(window.location.pathname)
+    }
 
     render() {
         return(
             <div>
                 <Router>
 
+                    {/* Sidebar will not appear for home, login and register */}
+                    {
+                        window.location.pathname !== '/' &&
+                        window.location.pathname !== '/login' &&
+                        window.location.pathname !== '/register' &&
+                        <Sidebar/>
+                     }
+
                     <Switch>
                         <Route path="/" exact component={Home}/>
+                        <Route path="/dashboard" exact component={Dashboard}/>
+
+                        <Route path="/user" exact component={Users}/>
+                        <Route path="/classroom" exact component={Classroom}/>
+                        <Route path="/library" exact component={ViewLibrary}/>
+                        <Route path="/notice" exact component={Notices}/>
                     </Switch>
 
                 </Router>
