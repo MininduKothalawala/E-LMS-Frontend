9d36a9e05ea48ffc48be2a597a464d2aa8f2b979
Isuri Amasha <gph.isuriamasha@gmail.com>
2021-08-26 14:32:18 -0700

    Fifth commit of classroom management

--- src/Components/Classroom/Classroom-List-Student.js
+++ src/Components/Classroom/Classroom-List-Student.js
@@ -1,13 +1,13 @@
 import React, {Component} from "react";
-import {Link, withRouter} from "react-router-dom";
 import axios from "axios";
-import {Container, Card, Row, Image, Col, Form, Badge} from "react-bootstrap";
-import "../../Stylesheets/Student-Teacher-UI-styles.css"
-import cardImg from "../../Assets/404-Pages.jpg"
-import {faCalendar, faCalendarAlt, faEdit} from "@fortawesome/free-solid-svg-icons";
+import {Card, Row, Col, Badge, Modal} from "react-bootstrap";
+import {faCalendarAlt} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faClock} from "@fortawesome/free-regular-svg-icons";
 import moment from "moment";
+import "../../Stylesheets/Student-Teacher-UI-styles.css"
+import ClassroomDetailsAdmin from "./Classroom-Details-Admin";
+import ClassroomDetailsStudent from "./Classroom-Details-Student";
 
 
 class ClassroomListStudent extends Component {
@@ -17,7 +17,8 @@ class ClassroomListStudent extends Component {
 
         this.state = {
             id: '',
-            classrooms: []
+            classrooms: [],
+            show: false
         }
     }
     componentDidMount() {
@@ -33,7 +34,22 @@ class ClassroomListStudent extends Component {
     }
 
     gotoDetails = (id) => {
-        this.props.history.push(`/adminClassDetails/`+id)
+        // set show:true so the modal box will be visible
+        // set id to pass it to the next component through props,
+
+        this.setState({
+            id: id,
+            show: true
+        })
+    }
+
+    //Modal box
+    showModalBox = () => {
+        this.setState({show: true})
+    }
+    //Modal box
+    closeModalBox = () => {
+        this.setState({show: false})
     }
 
     render() {
@@ -46,18 +62,18 @@ class ClassroomListStudent extends Component {
 
                     {/*--------------------------- Search ---------------------------*/}
                     <Col className={"px-0"} xl={3}>
-                        <Form>
-                            <Form.Group controlId={"formResourceGrade"}>
-                                <Form.Select>
-                                    <option>Select grade</option>
-                                    <option value={1}>Grade 1</option>
-                                    <option value={2}>Grade 2</option>
-                                    <option value={3}>Grade 3</option>
-                                    <option value={4}>Grade 4</option>
-                                    <option value={5}>Grade 5</option>
-                                </Form.Select>
-                            </Form.Group>
-                        </Form>
+                        {/*<Form>*/}
+                        {/*    <Form.Group controlId={"formResourceGrade"}>*/}
+                        {/*        <Form.Select>*/}
+                        {/*            <option>Select grade</option>*/}
+                        {/*            <option value={1}>Grade 1</option>*/}
+                        {/*            <option value={2}>Grade 2</option>*/}
+                        {/*            <option value={3}>Grade 3</option>*/}
+                        {/*            <option value={4}>Grade 4</option>*/}
+                        {/*            <option value={5}>Grade 5</option>*/}
+                        {/*        </Form.Select>*/}
+                        {/*    </Form.Group>*/}
+                        {/*</Form>*/}
                     </Col>
                 </Row>
 
@@ -80,9 +96,9 @@ class ClassroomListStudent extends Component {
                                         <Col className={"text-start"}>{event.subject}</Col>
                                         <Col className={"text-end"}>{event.grade}</Col>
                                     </Row>
-                                    <Card.Img variant="top"
-                                              src={cardImg}
-                                        // src={event.classImg}
+                                    <Card.Img variant="top" className={"st-class-img"}
+                                              src={`http://localhost:8080/classroom/image/${event.img_fileId}`}
+
                                     />
                                     <Card.Body className={"px-3"}>
                                         <Card.Title className={"mb-2 st-card-title"}>{event.topic}</Card.Title>
@@ -102,63 +118,25 @@ class ClassroomListStudent extends Component {
                                     </Card.Body>
                                 </Card>
                             </Col>
-
                         )
                     }
                 </Row>
 
+
+                {/*------------------------ Modal Box for ViewMore Page ------------------------*/}
+                <Modal show={this.state.show} onHide={this.closeModalBox} centered fullscreen={"sm-down"} size={"lg"}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Classroom Details</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body className={"custom-modal-body-login p-0"}>
+                        <ClassroomDetailsStudent classId={this.state.id} />
+                    </Modal.Body>
+                </Modal>
+                {/*------------------------------------------------------------------------------*/}
+
             </div>
-
-
-
-
-            // <div >
-            //     <Card>
-            //     <Container>
-            //
-            //
-            //         <div className={"conference-outer-div"}>
-            //             {
-            //                 this.state.classrooms.length > 0 ?
-            //                     [
-            //                         <Row key={0} className={"conference-card-row"}>
-            //                             {
-            //                                 this.state.classrooms.map(event =>
-            //
-            //                                     <div className={"conference-card-col mb-4"} key={event.id}>
-            //                                         <div className={"conference-card"} key={event.id} onClick={() => this.gotoDetails(event.id)}>
-            //
-            //                                             <div className={"text-center image-card"}>
-            //                                                 <img alt={"card-background-image"} width={300}
-            //                                                      src={event.classImg}/>
-            //                                             </div>
-            //                                             <div className={"conference-card-body"}>
-            //                                                 <div><h5 className={"text-center"}>{event.grade}</h5>
-            //                                                     <h5 className={"text-center"}>{event.subject}</h5></div>
-            //
-            //                                                <div><h2 className={"text-center"}> {event.topic}</h2></div>
-            //                                                 <div><h4>By {event.addedBy}</h4></div>
-            //                                               <div><p className={"text-center"}>Date : {event.date}</p>
-            //                                                   <p className={"text-center"}>Time : {event.time}</p></div>
-            //                                             </div>
-            //                                         </div>
-            //                                     </div>
-            //                                 )
-            //                             }
-            //                         </Row>
-            //                     ]
-            //                     : <h1 className={"text-center my-5"}>No Classroom Available</h1>
-            //             }
-            //         </div>
-            //
-            //
-            //     </Container>
-            //     </Card>
-            // </div>
-
-
 
         )
     }
 }
-export default withRouter(ClassroomListStudent);
+export default ClassroomListStudent;
